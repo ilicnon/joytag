975b6cb01e87c08fb7e40cd1e850c91fb1fdd879
ilicnon <161084954+ilicnon@users.noreply.github.com>
2024-03-03 14:45:33 +0700

    proper modularization

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
 setup(
     name="joytag",
     version="0.1",
     packages=find_packages(),  
+    install_requires=requirements,
 )
