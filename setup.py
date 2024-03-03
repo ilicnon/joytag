c707d706099800e76df6852cc7ddd4d512ee2a06
ilicnon <161084954+ilicnon@users.noreply.github.com>
2024-03-03 14:37:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="joytag",
     version="0.1",
-    packages=find_packages(),
     package_dir={"": "."},
     packages=find_packages(where="."),  
 )
